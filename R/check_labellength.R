7c7e9a947bc331c5fa8f4e1761381c07891cc5da
lawrencehr <lawrence.rogers@hotmail.com>
2020-08-21 14:23:17 +1000

    Add label length check function

---
+++ R/check_labellength.R
@@ -0,0 +1,13 @@
+check_labellength <- function(label) {
+  #Before processing, check if passing a character vector
+  if(!is_character(label)) {
+    print("Error: 'label' is not a character vector.")
+  } else { #Check char
+    check <- tibble(label) %>%
+      rowwise() %>%
+      mutate(length = nchar(label)) %>%
+      filter(length >= 255)
+
+    print(check$label)
+  }
+}
