823102cd90d2e06f1c96bc66d6f022ddaddf966a
lawrencehr <lawrence.rogers@hotmail.com>
2020-08-24 14:55:24 +1000

    Testing roxygen function documentation features

--- R/check_labellength.R
+++ R/check_labellength.R
@@ -1,3 +1,14 @@
+#' ADA's standard label length check
+#'
+#' Checks a character vector for any rows that contain >= 255 characters.
+#'
+#' @param label character vector; a vector containing all variable labels from a dataset. Can be retrieved using \link[sjlabelled]{get_label}
+#'
+#' @return Returns a filtered tibble with strings over the 255 word limit, and their length count.
+#'
+#'
+#' @export
+
 check_labellength <- function(label) {
   # Before processing, check if passing a character vector
   if (!is_character(label)) {
